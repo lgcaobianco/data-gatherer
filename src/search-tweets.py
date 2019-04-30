f926a249767660f180f6f170328028feca4c52e6
lgcaobianco <gu.caobianco@gmail.com>
2019-04-30 16:05:02 -0300

    Added sentiment analysis of textblob and language options
    language option was added in order to remove languages such as kore
    an and others not supported by textblob's sentiment analysis

--- src/search-tweets.py
+++ src/search-tweets.py
@@ -6,16 +6,28 @@ from emoji import UNICODE_EMOJI
 import string
 from collections import Counter
 import pandas as pd
+from textblob import TextBlob
+from enum import Enum
 
 
-def searchTweetsAndWriteToFile(search_term, file_name):
+class LanguageEnum(Enum):
+    ENGLISH = "en"
+    PORTUGUESE = "pt"
+    SPANISH = "es"
+
+
+def searchTweetsAndWriteToFile(search_term, file_name, lang):
+    if(not isinstance(lang, LanguageEnum)):
+        raise TypeError("lang must be LanguageEnum instance")
+
     if(not len(search_term > 0)):
         return ""
     premium_search_args = load_credentials("~/.twitter_keys.yaml",
                                            yaml_key="search_tweets_30_day_dev",
                                            env_overwrite=False)
     # testing with a sandbox account
-    rule = gen_rule_payload(search_term, results_per_call=100)
+    rule = gen_rule_payload(search_term + " lang:" +
+                            lang.value, results_per_call=100)
     print(rule)
     tweets = collect_results(rule,
                              max_results=200,
@@ -56,10 +68,27 @@ def countWords(tweetList):
     return pd.DataFrame({'Unique Words': uniqueWords, 'Occurrences': occurrence})
 
 
+def get_sentiments(tweetList):
+    sentiments = list()
+    subjectivity = list()
+    textList = list()
+    for tweet in tweetList:
+        if(tweet.get("user").get("lang") == "en"):
+            text = tweet.get("text")
+            textList.append(text)
+            analysis = TextBlob(text)
+            sentiments.append(analysis.sentiment.polarity)
+            subjectivity.append(analysis.sentiment.subjectivity)
+    return pd.DataFrame(
+        {'Tweet text': textList, 'Sentiment': sentiments, 'Subjectivity': subjectivity})
+
+
 # read from file so i dont have to query again
 tweetList = readTweetsJsonFromFile('tweets.json')
 # switch everything to lowercase
 changeTextFromTweetsToLower(tweetList)
 # count the words and reorder from most used
 df = countWords(tweetList).sort_values(by=['Occurrences'], ascending=False)
+sentiments = get_sentiments(tweetList)
+print(sentiments.head())
 print(df.head(10))
